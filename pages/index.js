ce3b6d45a9f674e7292ba75e2e5431bdf458f152
Richard Ware <richard.ware@datacom.co.nz>
2019-04-01 13:34:51 +1300

    trying to fix deploy

--- pages/index.js
+++ pages/index.js
@@ -5,27 +5,12 @@ import * as React from "react";
 
 export default class Index extends React.Component {
 
-    constructor (props) {
+    constructor(props) {
         super(props)
         console.log('index:constructor');
     }
 
-    static async getInitialProps() {
-
-        console.log('index:getInitialProps');
-
-        const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
-        const data = await res.json()
-
-        console.log(`Show data fetched. Count: ${data.length}`)
-
-        return {
-            shows: data.map(entry => entry.show)
-        }
-    }
-
     render() {
-
 
 
         return (
--- server/index.js
+++ server/index.js
@@ -19,7 +19,27 @@ app.prepare().then(() => {
 
     // Pronto API
     server.use("/pronto", pronto);
+    server.get("/getJobs", (req, res) => {
+        console.log('server.js: api/pronto')
 
+        fetch('https://api.tvmaze.com/search/shows?q=batman', {
+            method: "GET",
+            headers: {
+                'X-Pronto-Token': req.get('X-Pronto-Token')
+            },
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (jsonData) {
+                res.setHeader('Content-Type', 'application/json')
+                res.end(JSON.stringify(jsonData))
+            })
+            .catch(err => {
+                res.redirect('/error');
+            });
+    });
+error
     server.get('*', (req, res) => {
         console.log('into nextjs');
         return handle(req, res)
