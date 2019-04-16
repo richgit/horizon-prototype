af949006d5f2d5e7187de20de8ce34dd1481b26c
Richard Ware <richard.ware@datacom.co.nz>
2019-04-17 11:56:07 +1200

    starting responsive header

--- components/Header.js
+++ components/Header.js
@@ -1,39 +1,36 @@
 import Link from 'next/link'
 
-const linkStyle = {
-    marginRight: 15
-}
-
 export default function Header() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark static-top bg-white border-bottom shadow-sm">
+        <nav className="navbar navbar-expand-lg navbar-light static-top border-bottom shadow-sm">
             <div className="container">
                 <a className="navbar-brand" href="#">
-                    <img src="../static/images/HEG-Logo.png" alt="HEG logo"/>
+                    <img src="../static/images/heg-logo-portal.png" alt="HEG logo"/>
                 </a>
+
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
+                    <ul className="navbar-nav ml-auto align-items-center">
+                        <li className="nav-item p-2">
                             <Link href="/index">
-                                <a style={linkStyle}>Home</a>
+                                <a className="text-dark">Home</a>
                             </Link>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item p-2">
                             <Link href="/reactiveJobs">
-                                <a style={linkStyle}>Reactive Jobs</a>
+                                <a className="text-dark">Reactive Jobs</a>
                             </Link>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item p-2">
                             <Link href="/login">
-                                <a className="btn btn-outline-primary" style={linkStyle}>Login</a>
+                                <a className="btn btn-outline-secondary">Login</a>
                             </Link>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item p-2">
                             <Link href="/logout">
-                                <a style={linkStyle}>Logout</a>
+                                <a className="btn btn-outline-secondary">Logout</a>
                             </Link>
                         </li>
                     </ul>
--- components/Layout.js
+++ components/Layout.js
@@ -12,12 +12,11 @@ export default function Layout(props) {
 
             <title>Horizon</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                  crossOrigin="anonymous"/>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-                    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-                    crossOrigin="anonymous"></script>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"/>
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js"></script>
+
+
             <Header/>
             {props.children}
         </div>
